2d86a346bd71cb0b1abbae062de487f64849fc17
Mansithakurrr <mansi.thakur@bigplutolabs.com>
2025-06-19 15:03:35 +0530

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -34,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
